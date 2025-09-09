2c7e0136323fb31e912c130b4276899db61aa955
Sander Bos <sander@streamhpc.com>
2025-09-09 21:44:11 +0200

    feat: add hello world default backend

--- grug/core.py
+++ grug/core.py
@@ -1,2 +1,8 @@
+try:
+    import backend  # try to load user-installed backend first
+except ImportError:
+    from . import backend  # fallback to bundled backend
+
 def hello():
     print("hello")
+    backend.do_work()
